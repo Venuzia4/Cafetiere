4a7c9637b231fa04a299809a98d72275372f818e
BABONGUI MABIKA Venuzia <vbabonguimabika@gmail.com>
2023-02-27 23:11:23 +0100

    wip

--- cafe/src/main/java/exo4/Main.java
+++ cafe/src/main/java/exo4/Main.java
@@ -4,66 +4,68 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        Restaurant resto = new Restaurant();
-        TypeCafe[] typesCafe = TypeCafe.values();
-        String nomClient = JOptionPane.showInputDialog(null, "Bonjour ! Quel est votre nom ?");
+        Restaurant restaurant = new Restaurant();
+        String nomClient = JOptionPane.showInputDialog(null, "Bonjour, quel est votre nom ?");
 
-        JOptionPane.showMessageDialog(null, "Bonjour " + nomClient + " ! Bienvenue au restaurant.");
+        int reponseCommande = JOptionPane.showConfirmDialog(null, "Bonjour " + nomClient + ", puis-je prendre votre commande ?");
 
-        int choix = JOptionPane.showConfirmDialog(null, "Puis-je prendre votre commande, " + nomClient + " ?");
-        if (choix == JOptionPane.YES_OPTION) {
-            JOptionPane.showMessageDialog(null, "Très bien, je prends note de votre commande !");
-        } else {
-            JOptionPane.showMessageDialog(null, "D'accord, n'hésitez pas à me faire signe si vous avez besoin d'aide.");
-        }
+        if (reponseCommande == JOptionPane.YES_OPTION) {
 
-        String typeCafeChoisi = String.valueOf(JOptionPane.showInputDialog(null, "Quel est votre type de café ?", "Choix du café", JOptionPane.QUESTION_MESSAGE, null, typesCafe, typesCafe[0].toString()));
+            TypeCafe[] options = TypeCafe.values();
+            TypeCafe typeCafe = (TypeCafe) JOptionPane.showInputDialog(null, "Quel type de café désirez-vous ?", "Choix du café", JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
-        if (typeCafeChoisi.equals(TypeCafe.BATARD.toString())) {
-            JOptionPane.showMessageDialog(null, "Je suis désolé, vous n'êtes pas le bienvenu ici.");
-            System.exit(0);
-        }
-        int choixTasse = JOptionPane.showConfirmDialog(null, "Avez-vous votre propre tasse ?", "Choix de la tasse", JOptionPane.YES_NO_OPTION);
-        String tailleTasse = JOptionPane.showInputDialog(null, "Quelle est la taille de votre tasse (en ml) ?", "Taille de la tasse", JOptionPane.QUESTION_MESSAGE);
-        boolean aTasse = (choix == JOptionPane.YES_OPTION);
-
-        JSlider quantiteSlider = new JSlider(JSlider.HORIZONTAL, 0, 500, 0);
-        quantiteSlider.setMajorTickSpacing(50);
-        quantiteSlider.setPaintTicks(true);
-        quantiteSlider.setPaintLabels(true);
-
-        if (aTasse) {
-            String taille = JOptionPane.showInputDialog("Quelle taille pour votre café ?");
-           //client = new ClientAvecTasse(typeCafe, tailleTasse, quantiteCafe);
-        } else {
-            String typeCafe = JOptionPane.showInputDialog("Quel type de café voulez-vous ?");
-         // client = new ClientSansTasse(typeCafe,quantiteCafe);
-        }
-        Restaurant restaurant =new Restaurant();
-        int cout = restaurant.calculerPrix(client);
-        JOptionPane.showMessageDialog(null, "Le coût de votre commande est de " + cout + " centimes.");
-
-        int montantPaye = Integer.parseInt(JOptionPane.showInputDialog("Veuillez payer en centimes :"));
-
-        if (montantPaye == cout) {
-            JOptionPane.showMessageDialog(null, "Merci beaucoup ! Bonne dégustation !");
-        } else if (montantPaye > cout) {
-            int rendu = montantPaye - cout;
-            JOptionPane.showMessageDialog(null, "Merci beaucoup ! Voici votre monnaie : " + rendu + " centimes.");
-        } else {
-            int reste = cout - montantPaye;
-            int nbEssais = 0;
-            while (reste > 0 && nbEssais < 3) {
-                montantPaye = Integer.parseInt(JOptionPane.showInputDialog("Il manque " + reste + " centimes. Veuillez payer la somme restante :"));
-                reste -= montantPaye;
-                nbEssais++;
+            if (typeCafe == TypeCafe.BATARD) {
+                JOptionPane.showMessageDialog(null, "Désolé, nous ne servons pas de café bâtard !");
+                System.exit(0);
             }
-            if (reste == 0) {
-                JOptionPane.showMessageDialog(null, "Merci beaucoup ! Bonne dégustation !");
-            } else {
-                JOptionPane.showMessageDialog(null, "Au voleur, au voleur ! Mais que fait la police ?");
+            int reponseTasse = JOptionPane.showConfirmDialog(null, "Avez-vous votre propre tasse ?");
+            boolean avecTasse = (reponseTasse == JOptionPane.YES_OPTION);
+            int taille = 0;
+
+            if (avecTasse) {
+                String tailleStr = JOptionPane.showInputDialog(null, "Quelle est la taille de votre tasse ?");
+                taille = Integer.parseInt(tailleStr);
+            }
+            JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 500, 0);
+            slider.setMajorTickSpacing(50);
+            slider.setPaintTicks(true);
+            slider.setPaintLabels(true);
+
+            JOptionPane.showMessageDialog(null, slider, "Quantité de café", JOptionPane.QUESTION_MESSAGE);
+
+            int quantite = slider.getValue();
+
+            Cafe cafe = new Cafe(typeCafe, quantite);
+            Client client = new Client(nomClient, cafe, avecTasse);
+
+            restaurant.servir(client);
+            int somme = (int) client.valeurFacture;
+
+            while (true) {
+                String reponsePaiementStr = JOptionPane.showInputDialog(null, "Le total de votre facture est de " + somme + " centimes. Combien désirez-vous payer ?");
+                int reponsePaiement = Integer.parseInt(reponsePaiementStr);
+
+                if (reponsePaiement == somme) {
+                    JOptionPane.showMessageDialog(null, "Merci pour votre paiement, au revoir !");
+                    break;
+                } else if (reponsePaiement > somme) {
+                    int rendu = reponsePaiement - somme;
+                    JOptionPane.showMessageDialog(null, "Merci pour votre paiement, voici votre monnaie : " + rendu + " centimes. Au revoir !");
+                    break;
+                } else {
+                    somme -= reponsePaiement;
+                    JOptionPane.showMessageDialog(null, "Il reste " + somme + " centimes à payer.");
+                }
+
+                if (somme > 0) {
+                    JOptionPane.showMessageDialog(null, "Il reste " + somme + " centimes à payer.");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Au voleur, au voleur ! Mais que fait la police ?");
+                }
             }
         }
     }
+}
 
-    }
+
+
